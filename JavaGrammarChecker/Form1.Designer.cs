cddbb150637e8bd6c14934e0085514986dddd785
nidaeman1122 <nidaeman0987@gmail.com>
2024-11-06 05:12:49 -0800

    Add project files.

---
+++ JavaGrammarChecker/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace JavaGrammarChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.validateButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(195, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Java-like construct";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(391, 140);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(100, 20);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // validateButton
+            // 
+            this.validateButton.Location = new System.Drawing.Point(391, 198);
+            this.validateButton.Name = "validateButton";
+            this.validateButton.Size = new System.Drawing.Size(75, 23);
+            this.validateButton.TabIndex = 2;
+            this.validateButton.Text = "validateButton";
+            this.validateButton.UseVisualStyleBackColor = true;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(408, 295);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 13);
+            this.resultLabel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.validateButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button validateButton;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ JavaGrammarChecker/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace JavaGrammarChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // The missing Form1_Load event handler
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // This method runs when the form is loaded
+        }
+
+        // Button click handler to validate the input
+        private void validateButton_Click(object sender, EventArgs e)
+        {
+            string input = inputTextBox.Text;
+
+            // Call the validation function
+            if (IsValidJavaConstruct(input))
+            {
+                resultLabel.Text = "Valid Java construct!";
+                resultLabel.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                resultLabel.Text = "Invalid Java construct.";
+                resultLabel.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+        // Method to validate Java-like constructs using regular expressions
+        private bool IsValidJavaConstruct(string input)
+        {
+            // Define basic patterns for Java constructs
+
+            // Pattern for variable declarations like 'int x = 5;'
+            string variableDeclarationPattern = @"^\s*(int|float|double|String)\s+\w+\s*=\s*.+;\s*$";
+
+            // Pattern for method declarations like 'public void methodName() { }'
+            string methodDeclarationPattern = @"^\s*public\s+void\s+\w+\s*\(\)\s*\{\s*\}\s*$";
+
+            // Pattern for if-else statements like 'if (x > 0) { } else { }'
+            string ifElsePattern = @"^\s*if\s*\(.+\)\s*\{\s*\}\s*(else\s*\{\s*\})?\s*$";
+
+            // Check if input matches any of the patterns
+            return Regex.IsMatch(input, variableDeclarationPattern) ||
+                   Regex.IsMatch(input, methodDeclarationPattern) ||
+                   Regex.IsMatch(input, ifElsePattern);
+        }
+    }
+}
